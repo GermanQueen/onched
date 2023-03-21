2f1540fa047b044097855e089d97d855873979d9
GermanQueen <128449437+GermanQueen@users.noreply.github.com>
2023-03-21 05:47:55 +0100

    Add files via upload

---
+++ Onched.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Onched
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       GermanQueen
+// @match        https://www.jeuxvideo.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jeuxvideo.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    window.location.replace("https://onche.org/forum/1/blabla-general");
+})();
